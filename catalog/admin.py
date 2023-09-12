0af782c12ce4f699ac7b4e29c0c06cb3d250b3b6
Vlad Larin <lordgadyka71@gmail.com>
2023-09-12 16:43:30 +0300

    Add all models in catalog/admin.py
    Added Musician Instrument Genre Performance Band Song Album models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog.models import (
+    Musician,
+    Instrument,
+    Genre,
+    Performance,
+    Band,
+    Song,
+    Album
+)
+
+
+@admin.register(Musician)
+class MusicianAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "full_name",
+        "pseudonym",
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": (
+            "full_name",
+            "pseudonym",
+            "instruments",
+        )}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "full_name",
+                        "pseudonym",
+                        "instruments",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(Instrument)
+admin.site.register(Genre)
+admin.site.register(Performance)
+admin.site.register(Band)
+admin.site.register(Song)
+admin.site.register(Album)
