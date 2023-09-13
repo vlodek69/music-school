e5a6a252ae49c832ac153658504c69e318d35277
Vlad Larin <lordgadyka71@gmail.com>
2023-09-13 13:44:06 +0300

    Update index view in catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Band, Song
+
 
 def index(request):
-    return HttpResponse("Hello world!")
+    num_musicians = get_user_model().objects.count()
+    num_bands = Band.objects.count()
+    num_songs = Song.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_musicians": num_musicians,
+        "num_bands": num_bands,
+        "num_songs": num_songs,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "catalog/index.html", context=context)
