536ced90a46685974828bf73f1f506ad9ec8f92c
Vlad Larin <lordgadyka71@gmail.com>
2023-09-20 16:36:25 +0300

    Configure django-debug-toolbar

--- music_school/urls.py
+++ music_school/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
