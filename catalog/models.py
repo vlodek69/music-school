ffb53b905ec764abd29d46ffeb46a4060b0ed162
Vlad Larin <lordgadyka71@gmail.com>
2023-09-21 19:53:05 +0300

    Add uniqe=True parameter for name field in Instrument model

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class Instrument(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
