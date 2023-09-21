3200cf073ba57891a5de7fb6540a36f64f3615dc
Vlad Larin <lordgadyka71@gmail.com>
2023-09-21 19:50:58 +0300

    Add validate_unique_instruments_set in catalog.forms.PerformanceCreationForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.core.exceptions import ValidationError
 
 from catalog.models import Musician, Song, Performance, Instrument, Album, \
     Genre
@@ -34,6 +35,26 @@ class PerformanceCreationForm(forms.ModelForm):
     class Meta:
         model = Performance
         fields = "__all__"
+
+    def clean_instruments(self):
+        return validate_unique_instruments_set(
+            self.cleaned_data["musician"],
+            self.cleaned_data["instruments"]
+        )
+
+
+def validate_unique_instruments_set(musician, instruments):
+    instrument_sets = [
+        list(performance.instruments.all().order_by("id")) for performance in
+        Performance.objects.filter(musician=musician)
+    ]
+    print(list(instrument_sets))
+    print(list(instruments))
+    if list(instruments.order_by("id")) in instrument_sets:
+        raise ValidationError("Create unique performance for this musician")
+
+    return instruments
+
 
 
 class InstrumentCreationForm(forms.ModelForm):
