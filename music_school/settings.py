384b01b4de48f557302d983f88296c4fc0597039
Vlad Larin <lordgadyka71@gmail.com>
2023-10-24 18:33:01 +0300

    Update SECRET_KEY in settings.py

--- music_school/settings.py
+++ music_school/settings.py
@@ -30,7 +30,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
